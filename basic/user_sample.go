f277e33a134e94e2718027f1aeea5292bc550493
imran4u <er.imran4u@gmail.com>
2024-11-01 07:11:23 +0530

    use repeated and enum in proto buf

--- basic/user_sample.go
+++ basic/user_sample.go
@@ -8,5 +8,9 @@ import (
 func BasicUser() {
 	user := &basic.User{Id: 1, UserName: "Imran Ali", IsActive: true}
 	user.Password = []byte("mySecret")
+	user.Gender = basic.Gender_MALE
+	user.Emails = []string{"imran@ali", "imran@ali2"}
 	fmt.Println(user.String())
+	fmt.Println(user.String())
+	fmt.Println(user.Gender.String())
 }
